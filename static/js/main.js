95ff546baa292c41705ec35e7e9cd7119b0f1ceb
Taegyeong Han <65719512+Ronis-han@users.noreply.github.com>
2021-01-24 23:31:44 +0900

    header 모바일 메뉴 및 pc 메뉴 수정 및 배너 슬라이드 영역 작업  완료
    -window width 1240px 이하일때는 스크롤되도록 수정(풀페이지 스크롤 해제)
    (1240px 이상일때는 풀페이지 스크롤, 한화면씩 스크롤되도록 수정)
    -header 모바일 메뉴 추가
    -header pc 메뉴 추가
    -header 메뉴 모바일과 pc에서 충돌 버그 발견->모바일에서 준 style 초기화 시켜주고 pc 에서는 다시 갱신되도록 수정
    -모바일 일부 브라우저에서 스크롤시 화면이 늘어나는 버그 발견->초기 로드 될때만 높이 갱신되고 resize 때는 갱신되지 않도록 수정
    -헤더메뉴 및 배너 반응형 처리 완료

---
+++ static/js/main.js
@@ -0,0 +1,309 @@
+$(document).ready(function(){
+    var header=$('header');
+    var headerHeight=header.outerHeight();
+    var subHieght;
+    var gnb=header.find(".gnb>li");
+    var nav=$('header .gnb');
+    var trigger=$('.trigger');
+    var Timer;
+    //pc일때 풀페이지 구현
+    var sections=$('.main');
+    var  winWidth;
+    var wrap=$('.wrap');
+    var section=$('.section');
+    var lang=$('.right_menu .lang');
+    var fam=$('.right_menu .fam');
+    var subMenu=$('header .sub_con');
+    function fullpage(){
+            if(wrap.hasClass('pc')){
+                var currentindex=0;
+                var sectionLen=section.length;
+        
+                function setslide(){
+                    var prevHeight=0;
+                    var totalHeight=0;
+                    var currentHeight=0;
+                    var allHeight=0;
+                    section.each(function(index){
+                        currentHeight=$(this).outerHeight();
+                        allHeight +=currentHeight;
+                        prevHeight+=currentHeight;
+                        totalHeight=currentHeight+prevHeight-currentHeight*2;
+                        if(currentHeight != $(window).height()){
+                            totalHeight=currentHeight+prevHeight-currentHeight*2-($(window).height()-currentHeight);
+                          }
+                          console.log(currentHeight);
+                        $(this).attr('data-index',totalHeight);
+                    })
+                    sections.css({height:allHeight+'px'});
+                 
+                }//setslide
+        
+                function moveslide(index){
+                    if($('body').hasClass('hidden')) return;
+                    currentindex=index;
+                    if(currentindex <0){
+                        currentindex=0;
+                        return
+                    }
+                    if(currentindex >=sectionLen){
+                        currentindex=sectionLen-1;
+                        return
+                    }
+                    var sectionTop=section.eq(currentindex).attr('data-index');
+                    console.log(currentindex);
+                    sections.css(
+                        {transform:'translate3d(0,'+(-sectionTop)+'px'+',0)',
+                             });
+                }
+        
+                $('.nav-btn>li').click(function(){
+                    var index=$(this).index();
+                    moveslide(index);
+                })
+        
+                var wheellstate=false;
+                $('.section').on('mousewheel',function(e){
+                    console.log('마우스확인');
+                    var delta; 
+                    if (e.originalEvent.detail) {
+                        delta = e.originalEvent.detail;
+                    }
+                    else if (e.originalEvent.wheelDelta) {
+                        delta = e.originalEvent.wheelDelta;
+                    }
+                    else {
+                        delta = e.originalEvent.deltaY;
+                        delta = -delta;
+                    }
+            
+                    if (delta >= 0 && $('.pc').length>0) {
+                        if(!wheellstate){
+                        // if(currentindex<0) return;
+                        moveslide(currentindex-1)
+                        }
+                        wheellstate=setTimeout(function(){
+                            wheellstate=false;
+                        },700)
+                } else if (delta < 0 && $('.pc').length>0) {
+                    if(!wheellstate){
+                        // if(currentindex>sectionLen-1) return;
+                    moveslide(currentindex+1)
+                        }
+                        wheellstate=setTimeout(function(){
+                            wheellstate=false;
+                        },700)           
+                }
+                })
+            setslide();  
+            moveslide(currentindex);      
+
+        };
+    }
+
+    function init(){
+        winWidth=$(window).width();
+        var winHeight=$(window).height();
+        if(winWidth<1240){
+            console.log('모바일')
+            wrap.removeClass('pc');
+            sections.removeAttr("style");
+            section.removeAttr("data-index");         
+        }else{
+            console.log('pc');
+            wrap.addClass('pc');
+            section.removeAttr("style");
+            fullpage();
+        }
+    }
+
+    function activate(Elem){
+        gnb.removeClass('active');
+        Elem.addClass('active');
+    }
+    function inactivate(Elem){
+        Elem.removeClass('active');
+    }
+    /* header */
+    gnb.on('mouseenter focusin',function(){
+        if(winWidth>1221){
+            clearTimeout(Timer);
+            activate($('header'));
+            fam.add(lang).addClass('over');
+            subHieght=$(".gnb .sub_con").outerHeight();
+            var total=headerHeight+subHieght;
+            header.stop().animate({height:total+'px'},200)
+            subMenu.hide();
+            $(this).find(".sub_con").show();
+           }
+
+    })
+    .on('mouseleave focusout',function(){
+        if(winWidth>1221){
+            inactivate(header)
+            fam.add(lang).removeClass('over');
+            header.stop().animate({height:headerHeight+'px'},200)
+            subMenu.hide();
+            Timer=setTimeout(function(){
+                subMenu.hide();
+            },200)
+           }
+    })
+    /* 모바일 버전 메뉴 */
+    $('.gnb>li>a').click(function(){
+        if(winWidth<1220){
+                if($(this).hasClass('on')){
+                    subMenu.stop().slideUp(function(){
+                        $('.gnb>li>a').removeClass('on');
+                    });
+                }else{
+                    $('.gnb>li>a').removeClass('on');
+                    $(this).addClass('on');
+                    subMenu.stop().slideUp();
+                    $(this).parent('li').find('.sub_con').stop().slideDown();
+                }
+            
+
+        }
+        
+    })
+    $('.right_menu h2').click(function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass('active');
+        $(this).parent().find('.list').stop().slideToggle();
+    })
+    fam.add(lang).on('mouseleave',function(){
+        $(this).removeClass('active');
+        $(this).find('.list').stop().slideUp();
+    })
+    
+
+
+    trigger.click(function(){
+       if(winWidth<=1220){
+        $(this).add(nav).add('.right_menu').toggleClass('on');  
+        if(!$(this).hasClass('on')){
+            subMenu.stop().slideUp(100)
+            $('.gnb>li>a').removeClass('on');
+            $('body').removeClass("hidden");
+        }else{
+            $('body').addClass("hidden");
+        }
+       }else{
+           //pc
+           $(this).toggleClass('on');  
+           if((nav).add('.right_menu').hasClass("on")){
+            (nav).add('.right_menu').removeClass("on");
+           }
+           if(!$(this).hasClass('on')){
+            $('body').removeClass("hidden");
+           }else{
+            $('body').addClass("hidden");
+           }
+           if(!$('.pc_trigger_menu').hasClass("open")){
+            $('.pc_trigger_menu').addClass('open').removeClass('close');
+           }else{
+            $('.pc_trigger_menu').addClass('close').removeClass('open');
+           }
+      
+       
+       }
+
+    })
+
+    
+
+
+
+
+    /*banner 슬라이드 */
+    var loading=$('.banner .loading>li');
+    var playBtn=$('.banner .play-btn>a');
+    var swiper = new Swiper('.banner .swiper-container', {
+        speed: 1300,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true,
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+          },
+        slidesPerView:'1',
+        loop:true,
+        loopedSlides:1,
+        pagination: {
+          el: '.pager',
+          type: 'fraction',
+        },
+        navigation: {
+          nextEl: '.arrows .next',
+          prevEl: '.arrows .prev',
+        },
+        on: {
+            init: function () {
+                loading.eq(0).addClass('active');
+            },
+        }
+      })
+
+      swiper.on('slideChange', function() {
+          var index=swiper.realIndex;
+          loading.removeClass('active');
+          loading.eq(index).addClass('active');
+      });
+      
+      
+
+      loading.on('click focus',function(){
+          var index=$(this).index();
+          swiper.slideTo(index+1);
+          console.log(swiper.realIndex);
+          $(this).addClass('active').siblings('li').removeClass('active');
+      })
+      playBtn.on('click focus',function(){
+        $(this).toggleClass('on');
+        var icon=$(this).find('i');
+        if($(this).hasClass('on')){
+            swiper.autoplay.stop();
+            icon.attr('class','fas fa-play');
+        }else{
+            console.log('시작');
+            swiper.autoplay.start();
+            icon.attr('class','fas fa-pause');
+        }
+      })
+      //메뉴 리사이즈될때 메뉴 수정
+      function MenuResize(){
+        nav.removeClass('on');
+        $('.gnb>li>a').removeClass('on');
+        subMenu.hide();
+        subMenu.removeAttr('style')
+          if(winWidth<=1220){
+            $('.pc_trigger_menu').removeClass('open');
+            trigger.add(lang).add(fam).removeClass("on");
+            $(this).add(nav).add('.right_menu').removeClass('on'); 
+          }
+      }
+      function mobileHeight(){
+             var sec=$('section');
+             sec.css({height:$(window).height()+'px'});
+             console.log('높이수정');
+      }
+    
+   
+      
+    
+    $(window).on('resize',function(){
+        MenuResize();
+        init();
+    })
+
+    init();
+    
+    if(winWidth<1240){
+        mobileHeight();
+    }
+
+
+})//end
